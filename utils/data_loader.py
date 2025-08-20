6cd3b132e15991f75f47eb0d14d66f620720684e
Acorus137 <116312649+Acorus137@users.noreply.github.com>
2025-08-20 14:45:04 -0400

    Update data_loader.py
    corrected spelling of "Portfolio"

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_mls_data():
-    df = pd.read_csv("Porfolio/assets/MLS_Standings_1996-2025.csv")
+    df = pd.read_csv("Portfolio/assets/MLS_Standings_1996-2025.csv")
     df = df[df["Year"] < 2025]
     df.dropna(inplace=True)
     df.drop(columns=["Home", "Away", "Unnamed: 0"], inplace=True)
